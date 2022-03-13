41caf41dccd1cddb4c4e30b6d89870afc4080ede
hayder-anrepeat <hayder@andrepeat.io>
2022-03-13 17:06:07 +0100

    init

---
+++ schemas/topping.js
@@ -0,0 +1,33 @@
+import { FaPepperHot } from 'react-icons/fa';
+
+export default {
+  name: 'topping',
+  title: 'Toppings',
+  type: 'document',
+  icon: FaPepperHot,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'What is the name of the topping?',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '🥬' : ''}`,
+    }),
+  },
+};
